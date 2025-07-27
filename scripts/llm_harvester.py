35b3c630fe596c6d2b8b26effc8ab8558d23302f
IA990 <xsrsia990sensort@gmail.com>
2025-07-27 21:29:53 +0100

    Create llm_harvester.py

---
+++ scripts/llm_harvester.py
@@ -0,0 +1,2 @@
+# (voir code complet dans mes messages précédents, c'est celui générant les fiches Markdown)
+# Si tu veux un rappel du script complet, demande-moi simplement "affiche-moi le script complet".
